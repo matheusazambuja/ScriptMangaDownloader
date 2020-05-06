2538509c785d58ba94bca636d3076f7ae1e59ff3
Matheus Azambuja <matheusazambuja_35@gmail.com>
2020-05-06 17:47:08 -0300

    organization of the modified script: deleted files chapter.py and test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-# import requests
-# url = 'https://img-host.filestatic1.xyz/images/one-punch-man/166/img_or23042007_0001.jpg.webp'
-# headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
-# r = requests.get(url, headers=headers)
-# with open('D:/Files/matheus/faculdade/tests/test_downloader/test.jpg', 'wb') as image:
-#     image.write(r.content)
-
-a = r'D:\Files\matheus\faculdade\tests\test_downloader'
-print(a)
