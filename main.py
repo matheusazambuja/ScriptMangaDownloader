ccd89908274987a0e50af95f2aa4bed1a54bf140
Matheus Azambuja <matheusazambuja_35@gmail.com>
2020-08-30 01:23:17 -0300

    Edit function main

--- main.py
+++ main.py
@@ -2,14 +2,12 @@ import utils
 
 
 def main():
-    params = utils.get_configs()
-
-    utils.config_name(params['manga_name'])
-    params['manga_chapters'] = utils.config_chapters(params['manga_chapters'])
-    utils.config_path(params['manga_path'])
-
-    utils.run_script(params['manga_name'], params['manga_chapters'],
-                     params['manga_path'], params['manga_domain'])
+    config = utils.get_configs()
+    if not config['error']:
+        params = config['return']
+        utils.run_script(params['manga_name'], params['manga_chapters'], params['manga_path'])
+    else:
+        [print(e) for e in config['error']]
 
 
 if __name__ == '__main__':
