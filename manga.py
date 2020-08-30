160dfb20fcb8c34864ca0b48ed5fb5fdc50a912e
Matheus Azambuja <matheusazambuja_35@gmail.com>
2020-08-30 01:28:15 -0300

    Updated: site domain, functions and variable names

--- manga.py
+++ manga.py
@@ -2,8 +2,8 @@ import os
 import errno
 import re
 import utils
-from collections import OrderedDict
 from bs4 import BeautifulSoup
+from collections import OrderedDict
 
 
 class MangaDownload:
@@ -14,33 +14,32 @@ class MangaDownload:
 
     def find_manga_html(self):
         mangas = []
-        b_html = utils.request('https://mangahost.site/find/' +
-                               self.name.replace(' ', '+'))
+        b_html = utils.request('https://mangahosted.com/find/' + self.name.replace(' ', '+'))
         soup = BeautifulSoup(b_html.content, 'html.parser')
-        for tag in soup.find_all('h3', class_='entry-title'):
+        for tag in soup.find_all('h4', class_='entry-title'):
             tag = tag.find('a')
             tag = str(tag).replace('"', "'")
-            link = re.findall(r"(?<=<a href=')[^']*", str(tag))
             name = re.findall(r"(?<=title=')[^']*", str(tag))
-            mangas.append([name[0], link[0]])
+            link = re.findall(r"(?<=<a href=')[^']*", str(tag))
+            mangas.append({'title': name[0], 'link': link[0]})
+        mangas = utils.sort_mangas(mangas, query=self.name)
         return mangas
 
     def _select_manga(self):
         mangas = self.find_manga_html()
-        print('-----------------------------------------------')
-        print('Enter 0 to end execution.')
-        print('-----------------------------------------------')
         print(f'Found mangas:')
-        for manga in mangas:
-            print(f'{mangas.index(manga)+1}: {manga[0]}')
+        for i in range(len(mangas)):
+            print(f'{i+1}: {mangas[i]["title"]}')
+        print('-----------------------------------------------')
+        print('           Enter 0 to end execution')
+        print('-----------------------------------------------')
         code_manga = 'a'
-        while not code_manga.isdigit() \
-                or int(code_manga)-1 not in range(len(mangas)):
+        while not code_manga.isdigit() or int(code_manga)-1 not in range(len(mangas)):
             code_manga = input(f'Select a manga: ')
             if code_manga == '0':
                 exit(0)
-        self.name = mangas[int(code_manga)-1][0]
-        self.link = mangas[int(code_manga)-1][1]
+        self.name = mangas[int(code_manga)-1]['title']
+        self.link = mangas[int(code_manga)-1]['link']
 
 
 class Manga:
@@ -51,54 +50,60 @@ class Manga:
         self.path = path
 
     def find_chapters_html(self):
-        links_chapters = []
+        links_chapter = []
         b_html = utils.request(self.link)
         soup = BeautifulSoup(b_html.content, 'html.parser')
 
-        id_manga = re.search(r'mh\d+', b_html.url)
-        regex_links_chapters = r"\bhref=['\"](\S+" + \
-            re.escape(id_manga.group(0)) + r"\S+)\b"
-        links_chapters = re.findall(regex_links_chapters, str(soup))
-        links_chapters = list(OrderedDict.fromkeys(links_chapters))
+        manga_id = re.search(r'mh\d+', b_html.url)
+        links_chapter_regex = r"\bhref=['\"](\S+" + re.escape(manga_id.group(0)) + r"\S+)\b"
+        links_chapter = re.findall(links_chapter_regex, str(soup))
+        links_chapter = list(OrderedDict.fromkeys(links_chapter))
 
         chapters_return = []
-        links_chapters.reverse()
+        links_chapter.reverse()
         if self.chapters == (-1, -1):
-            find_first_manga = True
+            first_chapter_find = True
         else:
-            find_first_manga = False
-        for link in links_chapters:
-            regex_id_chapter = re.escape(
-                id_manga.group(0)) + r'/(\w+(.\w+)?)'
-            id_chapter = re.search(regex_id_chapter, link)
-            id_chapter = re.sub(
-                re.escape(id_manga.group(0)) + r'/', '', id_chapter.group(0))
-            if id_chapter.isdigit() and id_chapter == str(self.chapters[0]):
-                find_first_manga = True
-            if find_first_manga:
-                chapters_return.append([id_chapter, link])
-                if id_chapter.isdigit() and id_chapter == str(self.chapters[1]):
+            first_chapter_find = False
+        i = 0
+        while True:
+            chapter_id_regex = re.escape(manga_id.group(0)) + r'/(\w+(.\w+)?)'
+            chapter_id = re.search(chapter_id_regex, links_chapter[i])
+            chapter_id = re.sub(re.escape(manga_id.group(0)) + r'/', '', chapter_id.group(0))
+
+            # if chapter_id.isdigit():
+            #     if int(chapter_id) in range(self.chapters[0], self.chapters[-1]+1):
+            #         chapters_return.append({'id': chapter_id, 'link': links_chapter[i]})
+            #     elif int(chapter_id) > self.chapters[-1]:
+            #         break
+
+            if chapter_id.isdigit() and chapter_id == str(self.chapters[0]):
+                first_chapter_find = True
+            if first_chapter_find:
+                chapters_return.append({'id': chapter_id, 'link': links_chapter[i]})
+                if chapter_id.isdigit() and chapter_id == str(self.chapters[1]):
                     break
+            i += 1
         return chapters_return
 
     def download_chapters(self):
         chapters_num = self.find_chapters_html()
         for chapter in chapters_num:
-            print(chapter[0], chapter[1])
-            Chapter(self.name, chapter[0], chapter[1],
+            print(chapter['id'], chapter['link'])
+            Chapter(self.name, chapter['id'], chapter['link'],
                     self.path).download_pages()
 
 
 class Chapter():
-    def __init__(self, name, id_chapter, link, path):
+    def __init__(self, name, chapter_id, link, path):
         self.name = name
-        self.id_chapter = id_chapter
+        self.chapter_id = chapter_id
         self.link = link
         self.path = path
 
     def save_page(self, number_page, bin_image):
         manga_dir = self.path + '\\' + self.name + '\\' + \
-            str(self.id_chapter) + '\\' + str(number_page) + '.webp'
+            str(self.chapter_id) + '\\' + str(number_page) + '.webp'
 
         if not os.path.exists(os.path.dirname(manga_dir)):
             try:
@@ -111,22 +116,18 @@ class Chapter():
     def find_pages_html(self):
         pages = []
         b_html = utils.request(self.link)
-        soup = BeautifulSoup(b_html.content, 'html.parser')
-        regex_pages = r"\bsrc='(\w+://[\w.%+-/()]+)"
-        for tag in soup.find_all('script'):
-            pages = re.findall(regex_pages, str(tag))
-            if pages:
-                return pages
+        div_slider = BeautifulSoup(b_html.content, 'html.parser').find(name='div', attrs={'id': 'slider'})
+
+        return [tag.img['src'] for tag in div_slider.find_all(name='a')]
 
     def download_pages(self):
         print('-----------------------------------------------')
-        print(f'{self.name} - Chapter: {self.id_chapter}')
+        print(f'{self.name} - Chapter: {self.chapter_id}')
         links_pages = self.find_pages_html()
-        total_pages_download = 1
+        total_pages_download = 0
         if links_pages:
             print('Download started')
             for link in links_pages:
-                # print(link)
                 r = utils.request(link)
                 self.save_page(total_pages_download, r.content)
                 total_pages_download += 1
